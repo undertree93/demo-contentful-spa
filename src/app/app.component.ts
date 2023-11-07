ee4cb82c0469db5f2486be8f7aa9b92e0c7283d7
Leon <chen.l.leon@gmail.com>
2023-11-07 15:27:28 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contentful-spa';
+  data: any;
+  severity: string ='';
+
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe(data => {
+      this.data = data[0].fields.objectsReference[0].fields.englishContent;
+      this.severity = data[0].fields.objectsReference[0].fields.severity;
+
+    });
+  }
+
+  public getJSON(): Observable<any> {
+    return this.http.get('assets/api.json');
+  }
 }
